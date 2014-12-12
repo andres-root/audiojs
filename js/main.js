a273eca5716243bdff5971b588e5ca5e040d0ba4
andres-root <andres.root.coder@gmail.com>
2014-12-11 23:22:01 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,42 +1,44 @@
-// Wave Function
-var wave = (function (){
-	var canvas = document.getElementById("canvas");
-	var context = canvas.getContext('2d');
-	var x = 0;
-	var y = window.innerHeight / 2;
-	var radius = 1/2;
-    var frameRate = 10
-	context.fillStyle = "rgba(0, 0, 0, 1.0)";
-	context.fillRect(0, 0, canvas.width, canvas.height);
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-	context.beginPath();
+$(document).ready(function() {
+	// Wave Function
+	var wave = (function (){
+		var canvas = document.getElementById("canvas");
+		var context = canvas.getContext('2d');
+		var x = 0;
+		var y = window.innerHeight / 2;
+		var radius = 1/2;
+	    var frameRate = 10
+		context.fillStyle = "rgba(0, 0, 0, 1.0)";
+		context.fillRect(0, 0, canvas.width, canvas.height);
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+		context.beginPath();
 
 
-	var setUp = function () {
-	};
+		var setUp = function () {
+		};
 
-	var draw = function () {
-		context.arc(x, y, radius, 0, 2 * Math.PI, false);
-		context.fillStyle = '#0f0';
-		context.fill();
-	};
+		var draw = function () {
+			context.arc(x, y, radius, 0, 2 * Math.PI, false);
+			context.fillStyle = '#0f0';
+			context.fill();
+		};
 
-	var animate = function () {
-		draw();
-		if (x > canvas.width) {
-			x = 0;
-		} else {
-			x++;
+		var animate = function () {
+			draw();
+			if (x > canvas.width) {
+				x = 0;
+			} else {
+				x++;
+			}
 		}
-	}
 
-	var start = function () {
-    	setInterval(animate, frameRate)
-	}
+		var start = function () {
+	    	setInterval(animate, frameRate)
+		}
 
-	return {
-		start: start
-	}
-})();
-wave.start();
+		return {
+			start: start
+		}
+	})();
+	wave.start();
+});
